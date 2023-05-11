bbaa419f6b39703f3fa09602b30a17cbfe086312
Tsy <tangzh1983@gmail.com>
2023-05-11 23:14:59 +0900

    20230511

---
+++ GCity.RestAPI.FSharp/Controllers/IndexController.fs
@@ -0,0 +1,11 @@
+﻿namespace GCity.RestAPI.FSharp
+open Microsoft.AspNetCore.Mvc
+open Microsoft.Extensions.Logging
+
+[<ApiController>]
+[<Route("[controller]");Route("")>]
+type IndexController(logger : ILogger<IndexController>) =
+    inherit ControllerBase()
+
+    member _.Index() =
+        "This is Index Page"
